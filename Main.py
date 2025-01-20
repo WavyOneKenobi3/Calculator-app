bd84b57116a6cff9d4ee13737567b2db575a966d
Mark <19152780+WavyOneKenobi3@users.noreply.github.com>
2025-01-20 18:34:01 -0500

    Update Main.py

--- Main.py
+++ Main.py
@@ -12,32 +12,33 @@ def divide(a, b):
         
 def multi(a, b):
     return a * b
+
+def signal(num1, num2):
+    try:                        #put all of this into a their functions
+        num1 = int(input("First number? "))
+        num2 = int(input("Second number? "))
+        sign = input("Select +, -, /, or *: ")
+        if sign == "+":
+            print(f"{num1} {sign} {num2} = {add(num1, num2)}")
+        elif sign == "-":
+            print(f"{num1} {sign} {num2} = {sub(num1, num2)}")
+        elif sign == "*":
+            print(f"{num1} {sign} {num2} = {multi(num1, num2)}")
+        elif sign == "/":
+            print(f"{num1} {sign} {num2} = {divide(num1, num2)}")
+        else:
+            print("Invalid operator! Please select +, -, /, or *.")
+    except ValueError:
+        print("Invalid input! Please enter a number.")
         
 def Calc():
-    
-    
     while True:
-        try:
-            num1 = int(input("First number? "))
-            num2 = int(input("Second number? "))
-            sign = input("Select +, -, /, or *: ")
-        
-            if sign == "+":
-                print(f"{num1} {sign} {num2} = {add(num1, num2)}")
-            elif sign == "-":
-                print(f"{num1} {sign} {num2} = {sub(num1, num2)}")
-            elif sign == "*":
-                print(f"{num1} {sign} {num2} = {multi(num1, num2)}")
-            elif sign == "/":
-                print(f"{num1} {sign} {num2} = {divide(num1, num2)}")
-            else:
-                print("Invalid operator! Please select +, -, /, or *.")
-        except ValueError:
-            print("Invalid input! Please enter a number.")
-        
+        signal() #change this
+        pervious_num = 0
         next_input = input("Do you want to continue? Yes or No: ").lower()
         if next_input == "yes":
-            continue
+            
+            signal(num1 = )
         elif next_input == "no":
             break
             print("Goodbye!")
