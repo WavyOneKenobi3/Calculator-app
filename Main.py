023e20c9c19093868aa235b838075cf7dcffc5de
Mark <19152780+WavyOneKenobi3@users.noreply.github.com>
2025-01-24 11:41:32 -0500

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,5 +1,6 @@
 # Calculator
-operatior = { }
+
+
 def add(a, b):
     return a + b
         
@@ -14,39 +15,35 @@ def divide(a, b):
 def multi(a, b):
     return a * b
 
-num1 = int(input("First number? "))
-num2 = int(input("Second number? "))
-sign = input("Select +, -, /, or *: ")
-        
+operator = {
+    "+" : add,
+    "-" : sub, 
+    "*" : multi, 
+    "/": divide
+ }
 
-def signal(num1, num2):
-    try:                        #put all of this into a their functions
-        if sign == "+":
-            print(f"{num1} {sign} {num2} = {add(num1, num2)}")
-        elif sign == "-":
-            print(f"{num1} {sign} {num2} = {sub(num1, num2)}")
-        elif sign == "*":
-            print(f"{num1} {sign} {num2} = {multi(num1, num2)}")
-        elif sign == "/":
-            print(f"{num1} {sign} {num2} = {divide(num1, num2)}")
-        else:
-            print("Invalid operator! Please select +, -, /, or *.")
-    except ValueError:
-        print("Invalid input! Please enter a number.")
+def calculator():
+    num1 = int(input("First number? "))
+
+    for symbol in operator:
+        print(symbol)
+    should_continue = True  #set a flag
+
+    while should_continue:
+        sign = input("Select an Operator. ")
+        num2 = int(input("Second number? "))
+        calc_funciton = operator[sign]
+        answer = calc_funciton(num1, num2)
+        print(f"{num1} {sign} {num2} = {answer}")
         
-def Calc():
-    while True:
-        signal() #change this
-        pervious_num = 0
-        next_input = input("Do you want to continue? Yes or No: ").lower()
-        if next_input == "yes":
-            
-            signal(num1 = )
-        elif next_input == "no":
-            break
-            print("Goodbye!")
-        else:
-            print("Invalid response! Exiting.")
+        ask_continue = input("do you want to continue with the same number? Yes or No if you want to start over? Type End to end program  ").lower()
+        if ask_continue == "yes":
+            num1 = answer
+        elif ask_continue == "no":
+            calculator()
+        elif ask_continue == "end":
+            should_continue == False
             break
 
-Calc()
+
+calculator()
